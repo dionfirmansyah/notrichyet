e0cceff5640f0eba673c65b6933f7d054823983b
dionfirmansyah <dioonfirmansyah@gmail.com>
2025-08-17 14:06:36 +0800

    first commit

---
+++ src/pages/Add/Add.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { AddForm } from './components';
+
+export function Add() {
+    return <AddForm />;
+}
---
+++ src/pages/Hangout/Hangout.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { HangoutForm } from './components';
+
+export function Hangout() {
+    return <HangoutForm />;
+}
